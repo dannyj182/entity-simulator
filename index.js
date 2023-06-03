4b203c1611918c3259e8ae038620f93faee95eda
Danny Alexander Jimenez Casares <dannyj182@gmail.com>
2023-06-03 11:25:28 -0300

    Cambios en index.js

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
 const express = require('express');
 const axios = require('axios');
-
 const app = express();
 const PORT = process.env.PORT || 8080; // Puerto en el que se ejecutará el servidor Express
-const TIEMPO = 60000; // 1 minuto
+const TIEMPO = 60000; // Acá se define con que frecuencia se ejecutará el método executeMultipleCallbacks()
+const HTTP_PORT = 7897; // Puerto del agente IoT
+const APIKEY = '4jggokgpepnvsb2uv4s40d59ov'; // apikey usado cuando se crea el service group del agente IoT
+const ENTITY_TYPE = 'AirQualityObserved'; // entity_type usado cuando se crea el service group del agente IoT
+const ENTITY_ID = ['001', '002', '003', '004', '005', '006', '007', '008', '009', '010']; // Los numeros de las entidades
 
 function generateDataString() {
     function getRandomNumber(min, max) {
         return Math.random() * (max - min) + min;
     }
-
     const pm1 = getRandomNumber(0, 100).toFixed(2);
     const pm10 = getRandomNumber(0, 200).toFixed(2);
     const pm25 = getRandomNumber(0, 100).toFixed(2);
@@ -22,7 +24,7 @@ function generateDataString() {
 async function executeCallback(number) {
     try {
         const data = generateDataString();
-        const url = `http://localhost:7897/iot/d?k=4jggokgpepnvsb2uv4s40d59ov&i=AirQualityObserved00${number}`;
+        const url = `http://localhost:${HTTP_PORT}/iot/d?k=${APIKEY}&i=${ENTITY_TYPE}${number}`;
         const contentType = 'text/plain';
 
         const response = await axios.post(url, data, {
@@ -30,6 +32,7 @@ async function executeCallback(number) {
                 'Content-Type': contentType,
             },
         });
+        
         console.log(new Date().toLocaleString(), response.status, response.statusText);
 
     } catch (error) {
@@ -38,7 +41,7 @@ async function executeCallback(number) {
 }
 
 async function executeMultipleCallbacks() {
-    for (let i = 0; i <= 9; i++) {
+    for (let i of ENTITY_ID) {
         await executeCallback(i);
     }
 }
